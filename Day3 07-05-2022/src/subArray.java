55779a5055e447d338aa8b48ff7286cec8b0b4ae
jagad-brijesh <103950643+jagad-brijesh@users.noreply.github.com>
2022-05-11 21:25:51 +0530

    Add files via upload

---
+++ Day3 07-05-2022/src/subArray.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class subArray
+{
+	static void allSubset(int arr[], int n) 
+	{ 
+	    int subset_size = (int)Math.pow(2, n);
+	    int i, j;
+	    ArrayList<ArrayList<Integer>> final_subset = new ArrayList<>();
+	    for(i=1;i<subset_size;i++) 
+	    {
+	        ArrayList<Integer> subset = new ArrayList<>();
+	        for(j=0;j<n;j++)
+	        { 
+	            // Check if jth bit in the i is set
+	            // If set then add jth element to the subset
+	            if((i & (1 << j)) != 0)
+	                subset.add(arr[j]);
+	        } 
+	       	final_subset.add(subset);
+	    }
+	    System.out.print(final_subset);
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter Size of Set:");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.print("Enter set elements:");
+		for (int i=0;i<n ;i++ ) {
+			arr[i] = sc.nextInt();
+		}
+		allSubset(arr,n);
+	}
+}
